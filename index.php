4d3739b0c5364c21898cd9d15d22301857e76e30
unknown <khurshidengineer@gmail.com>
2015-10-07 07:21:11 +0600

    index file

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html >
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+	<title>User Panel </title>
+	<style>
+	body{background-color:#CC7A00;}
+	</style>
+</head>
+	
+	<body>
+	<h2>User Panel</h2>
+	
+	<li><a href="signup.php">Sign Up</li>
+	<li><a href="login.php">Login</li>
+	
+	
+	</body>
+</html>
